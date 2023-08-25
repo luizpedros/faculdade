5ffaa7242a16092e1667a4fba2429f031766efa5
luizpedros <luizpedro.pedrosilva@gmail.com>
2023-08-25 11:47:13 -0300

    Usando WHILE

---
+++ somaAte10comWHILE.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    int valor= 0;
+
+    while (valor <= 10)
+    {
+        printf("\n valor = %d", valor);
+        valor++;
+    }
+    
+
+}
